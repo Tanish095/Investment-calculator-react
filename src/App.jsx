a9544f375fe50f1a5eb14d9dcd6f40c8219ef9bd
Tanish <tanishkhullar95@gmail.com>
2024-07-04 19:36:53 +0530

    investment calculator-using react

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import Header from "./components/Header.jsx";
+import Results from "./components/Results.jsx";
+import UserInput from "./components/UserInput.jsx";
+import { useState } from "react";
+function App() {
+  const [userInput,setUserInput] = useState({
+    initialInvestment: 10000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10,
+  });
+
+  const inputIsValid = userInput.duration>=1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput(prevUserInput => {
+        return {
+            ...prevUserInput,
+            [inputIdentifier]: +newValue
+        };
+    });
+}
+  return (<div>
+    <Header/>
+    <UserInput onChange={handleChange} userInput={userInput}/>
+    {!inputIsValid && <p className="center">Please enter a duration greater than 0</p>}
+    {inputIsValid && <Results input={userInput}/>}
+    </div>
+  )
+}
+
+export default App
